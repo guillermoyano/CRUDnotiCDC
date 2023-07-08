abc4b9ae2633d26e13bec9187cc87a721074c973
María Inés A. F <94498638+minesaba@users.noreply.github.com>
2023-07-07 23:38:56 -0300

    Add files via upload

--- static/JS/producto_editar.js
+++ static/JS/producto_editar.js
@@ -16,23 +16,29 @@ console.log(parts)
 //// [[“id",3] , [“nombre",’tv50’]]
 //decodeUriComponent elimina los caracteres especiales que recibe en la URL 
 document.getElementById("id").value = decodeURIComponent(parts[0][1])
-document.getElementById("nombre").value = decodeURIComponent(parts[1][1])
-document.getElementById("precio").value = decodeURIComponent(parts[2][1])
-document.getElementById("stock").value =decodeURIComponent( parts[3][1])
-document.getElementById("imagen").value =decodeURIComponent( parts[4][1])
+document.getElementById("titular").value = decodeURIComponent(parts[1][1])
+document.getElementById("autor").value = decodeURIComponent(parts[2][1])
+document.getElementById("fecha").value =decodeURIComponent( parts[3][1])
+document.getElementById("copete").value =decodeURIComponent( parts[4][1])
+document.getElementById("cuerpo").value =decodeURIComponent( parts[5][1])
+document.getElementById("imagen").value =decodeURIComponent( parts[6][1])
 
 function modificar() {
     let id = document.getElementById("id").value
-    let n = document.getElementById("nombre").value
-    let p = parseFloat(document.getElementById("precio").value)
-    let s = parseInt(document.getElementById("stock").value)
+    let t = document.getElementById("titular").value
+    let a = document.getElementById("autor").value
+    let f = document.getElementById("fecha").value
+    let co = document.getElementById("copete").value
+    let cu = document.getElementById("cuerpo").value
     let i = document.getElementById("imagen").value
    
     let producto = {
-        nombre: n,
-        precio: p,
-        stock: s,
-        imagen:i
+        titular: t,
+        autor: a,
+        fecha: f,
+        copete: co,
+        cuerpo: cu,
+        imagen: i
     }
     let url = "http://localhost:5000/productos/"+id
     var options = {
